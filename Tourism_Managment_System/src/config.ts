5a78ad8588532083e74b7a0c6daa3ec85996c237
diludfernando <diludpramuditha@gmail.com>
2026-05-03 12:33:43 +0530

    fixed the frontend feching error

--- Tourism_Managment_System/src/config.ts
+++ Tourism_Managment_System/src/config.ts
@@ -19,4 +19,4 @@ const lanBase = host ? `http://${host}:5000` : undefined;
 const emulatorBase =
   Platform.OS === 'android' && !Constants.isDevice ? androidEmulatorBase : undefined;
 
-export const API_BASE = extraApiBase || emulatorBase || lanBase || defaultBase;
+export const API_BASE = extraApiBase || emulatorBase || defaultBase || lanBase;
